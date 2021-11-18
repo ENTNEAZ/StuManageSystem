c2cc24b9f9be2c1615618e3383663f6705bdfe11
ENTNEAZ <langyuez@outlook.com>
2021-11-18 22:09:34 +0800

    制表符问题

--- C++Homework.cpp
+++ C++Homework.cpp
@@ -4,20 +4,20 @@
 
 /*struct Date
 {
-    int year;
-    int month;
-    int day;
+	int year;
+	int month;
+	int day;
 };*/
 
 struct Stu {
-    int id;//学号 
-    char name[15];//姓名 
-    char sex[5]; //性别 
-    char field[30];//专业 
-    char birthday[20];//出生日期 
-    char address[100];//家庭地址 
-    float E_grade;//英语入学成绩 
-    struct Stu* next;
+	int id;//学号 
+	char name[15];//姓名 
+	char sex[5]; //性别 
+	char field[30];//专业 
+	char birthday[20];//出生日期 
+	char address[100];//家庭地址 
+	float E_grade;//英语入学成绩 
+	struct Stu* next;
 };
 
 //有新生来报到，要逐个录入其信息，
@@ -40,7 +40,7 @@ struct Stu* p, * q;
 void menu();		//菜单 0 
 void add();			//新增学生信息 done 1. 
 void del();			//删除学生信息 done 2.
-                    //TODO 导入学生信息 3.
+					//TODO 导入学生信息 3.
 void search();		//学生信息搜索(按姓名)  done 4.
 void searchall();	//学生信息统计（按专业或性别或年龄---年龄要自动计算） 5.
 void sort();		//排序 6.
@@ -52,241 +52,241 @@ void change();//修改学生信息（好像没要求写，自己加的）done
 bool isExist(int id,bool output);
 
 void menu() {
-    int userChoice;
-    while (true) {
-        system("cls");
-        printf("\n");
-        printf("----------------------------学生基本信息管理系统----------------------------\n");
-        printf("\n");
-        printf("1.新增学生信息输入 \n");
-        printf("2.查询学生信息输入 \n");
-        printf("3.修改学生信息输入 \n");
-        printf("4.删除学生信息输入 \n");
-        printf("5.浏览全部信息输入 \n");
-        printf("6.学生成绩排序输入 \n");
-        printf("7.学生信息储存输入 \n");
-        printf("8.保存并安全退出   \n");
-        printf("\n");
-        printf("-----------------------------------------------------------------------------\n");
-        printf("请输入序号：");
-        scanf("%d", &userChoice);
-        switch (userChoice)
-        {
-        case 1:
-            add();
-            break;
-        case 2:
-            search();
-            break;
-        case 3:
-            change();
-            break;
-        case 4:
-            del();
-            break;
-        case 5:
-            searchall();
-            break;
-        case 6:
-            sort();
-            break;
-        case 7:
-            save();
-            break;
-        case 8:
-            printf("退出");
-        default:
-            printf("重新输入\n");
-        }
-    }
+	int userChoice;
+	while (true) {
+		system("cls");
+		printf("\n");
+		printf("----------------------------学生基本信息管理系统----------------------------\n");
+		printf("\n");
+		printf("1.新增学生信息输入 \n");
+		printf("2.查询学生信息输入 \n");
+		printf("3.修改学生信息输入 \n");
+		printf("4.删除学生信息输入 \n");
+		printf("5.浏览全部信息输入 \n");
+		printf("6.学生成绩排序输入 \n");
+		printf("7.学生信息储存输入 \n");
+		printf("8.保存并安全退出   \n");
+		printf("\n");
+		printf("-----------------------------------------------------------------------------\n");
+		printf("请输入序号：");
+		scanf("%d", &userChoice);
+		switch (userChoice)
+		{
+		case 1:
+			add();
+			break;
+		case 2:
+			search();
+			break;
+		case 3:
+			change();
+			break;
+		case 4:
+			del();
+			break;
+		case 5:
+			searchall();
+			break;
+		case 6:
+			sort();
+			break;
+		case 7:
+			save();
+			break;
+		case 8:
+			printf("退出");
+		default:
+			printf("重新输入\n");
+		}
+	}
 }
 
 void add()//增加 
 {
-    int id;
-    struct Stu* toAdd;
-    struct Stu* a = head;
-    printf("输入id：");
-    scanf("%d", &id);
-    if (isExist(id, true)) {
-        add();//已经存在此人 递归再次输入
-    } else {
-        toAdd = (struct Stu*)malloc(sizeof(struct Stu));
-        toAdd->id = id;
-        printf("输入姓名: ");
-        scanf("%s", &toAdd->name);
-        printf("输入性别: ");
-        scanf("%s", &toAdd->sex);
-        printf("输入专业: ");
-        scanf("%s", &toAdd->field);
-        printf("输入出生日期: ");
-        scanf("%d", &toAdd->birthday);
-        printf("输入家庭地址: ");
-        scanf("%s", &toAdd->address);
-        printf("输入英语入学成绩：");
-        scanf("%f", &toAdd->E_grade);
-        toAdd->next = NULL;
+	int id;
+	struct Stu* toAdd;
+	struct Stu* a = head;
+	printf("输入id：");
+	scanf("%d", &id);
+	if (isExist(id, true)) {
+		add();//已经存在此人 递归再次输入
+	} else {
+		toAdd = (struct Stu*)malloc(sizeof(struct Stu));
+		toAdd->id = id;
+		printf("输入姓名: ");
+		scanf("%s", &toAdd->name);
+		printf("输入性别: ");
+		scanf("%s", &toAdd->sex);
+		printf("输入专业: ");
+		scanf("%s", &toAdd->field);
+		printf("输入出生日期: ");
+		scanf("%d", &toAdd->birthday);
+		printf("输入家庭地址: ");
+		scanf("%s", &toAdd->address);
+		printf("输入英语入学成绩：");
+		scanf("%f", &toAdd->E_grade);
+		toAdd->next = NULL;
 
-        if (head == NULL) {
-            head = toAdd;
-            last = head;//last指向最后一个元素
-        }
-        else {
-            last->next = toAdd;
-            last = toAdd;
-        }
-    }
-    return;
+		if (head == NULL) {
+			head = toAdd;
+			last = head;//last指向最后一个元素
+		}
+		else {
+			last->next = toAdd;
+			last = toAdd;
+		}
+	}
+	return;
 }
 
 bool isExist(int id,bool output = false)//查重 output为是否输出已经存在的信息
 {
-    if (head == NULL)
-        return false;
-    struct Stu* item = head;
-    if (item->id == id)
-    {
-        if (output) {
-            printf("id: %d   姓名: %s   性别: %s   专业: %s   \n", item->id, item->name, item->sex, item->field);
-            printf("学号已存在，请重新输入\n");
-            printf("\n");
-        }
-        return true;
-    }
-    while (item != NULL)
-    {
-        if (item->id == id)
-        {
-            if (output) {
-                printf("id: %d   姓名: %s   性别: %s   专业: %s   \n", item->id, item->name,item->sex, item->field);
-                printf("学号已存在，请重新输入\n");
-                printf("\n");
-            }
-            return true;
-        }
-        else
-            item = item -> next;
-    }
-    return false;
+	if (head == NULL)
+		return false;
+	struct Stu* item = head;
+	if (item->id == id)
+	{
+		if (output) {
+			printf("id: %d   姓名: %s   性别: %s   专业: %s   \n", item->id, item->name, item->sex, item->field);
+			printf("学号已存在，请重新输入\n");
+			printf("\n");
+		}
+		return true;
+	}
+	while (item != NULL)
+	{
+		if (item->id == id)
+		{
+			if (output) {
+				printf("id: %d   姓名: %s   性别: %s   专业: %s   \n", item->id, item->name,item->sex, item->field);
+				printf("学号已存在，请重新输入\n");
+				printf("\n");
+			}
+			return true;
+		}
+		else
+			item = item -> next;
+	}
+	return false;
 }
 
 void search()//查询 
 {
-    struct Stu* item = head;
-    char inputName[15];
-    printf("输入要查询学生的姓名:");
-    scanf("%s", &inputName);
-    inputName[14] = '\0';//防止用户输入过长 导致没有\0
-    printf("下面是数据库内有关\"%s\"的信息\n\n", inputName);
-    while (item != NULL)
-    {
-        if (strcmp(item->name,inputName) == 0)
-        {
-            printf("id: %d   姓名: %s   性别: %s   专业: %s   出生日期: %s   家庭地址: %s   英语入学成绩: %f\n", item->id, item->name, item->sex, item->field, item->birthday, item->address, item->E_grade);
-            //防止重名 继续执行
-            item = item->next;
-        }
-        else {
-            item = item->next;
-        }
-    }
-    printf("\n以上是数据库内有关\"%s\"的信息\n", inputName);
-    system("pause");
-    return;
+	struct Stu* item = head;
+	char inputName[15];
+	printf("输入要查询学生的姓名:");
+	scanf("%s", &inputName);
+	inputName[14] = '\0';//防止用户输入过长 导致没有\0
+	printf("下面是数据库内有关\"%s\"的信息\n\n", inputName);
+	while (item != NULL)
+	{
+		if (strcmp(item->name,inputName) == 0)
+		{
+			printf("id: %d   姓名: %s   性别: %s   专业: %s   出生日期: %s   家庭地址: %s   英语入学成绩: %f\n", item->id, item->name, item->sex, item->field, item->birthday, item->address, item->E_grade);
+			//防止重名 继续执行
+			item = item->next;
+		}
+		else {
+			item = item->next;
+		}
+	}
+	printf("\n以上是数据库内有关\"%s\"的信息\n", inputName);
+	system("pause");
+	return;
 }
 
 void change()//修改 
 {
-    struct Stu* item;
-    int id;
-    printf("输入要修改学生的id:");
-    scanf("%d", &id);
-    item = head;
-    while (item != NULL)
-    {
-        if (item->id == id)
-        {
-            printf("开始修改\n");
-            printf("输入学号:");
-            scanf("%d", &item->id);
-            printf("输入姓名:");
-            scanf("%s", &item->name);
-            printf("输入性别:");
-            scanf("%s", &item->sex);
-            printf("输入专业:");
-            scanf("%s", &item->field);
-            printf("输入出生日期:");
-            scanf("%d", &item->birthday);
-            printf("输入家庭地址:");
-            scanf("%s", &item->address);
-            printf("输入英语入学成绩:");
-            scanf("%f", &item->E_grade);
-            printf("-----------------------------------------------");
-            printf("\n");
-            printf("修改后结果为\n");
-            printf("id: %d   姓名: %s   性别: %s   专业: %s   出生日期: %s   家庭地址: %s   英语入学成绩: %f\n", item->id, item->name, item->sex, item->field, item->birthday, item->address, item->E_grade);
-            printf("\n");
-            system("pause");
-            return;
-        }
-        else
-            item = item->next;
-    }
-    printf("id不存在！返回至菜单\n");
-    system("pause");
-    return;
+	struct Stu* item;
+	int id;
+	printf("输入要修改学生的id:");
+	scanf("%d", &id);
+	item = head;
+	while (item != NULL)
+	{
+		if (item->id == id)
+		{
+			printf("开始修改\n");
+			printf("输入学号:");
+			scanf("%d", &item->id);
+			printf("输入姓名:");
+			scanf("%s", &item->name);
+			printf("输入性别:");
+			scanf("%s", &item->sex);
+			printf("输入专业:");
+			scanf("%s", &item->field);
+			printf("输入出生日期:");
+			scanf("%d", &item->birthday);
+			printf("输入家庭地址:");
+			scanf("%s", &item->address);
+			printf("输入英语入学成绩:");
+			scanf("%f", &item->E_grade);
+			printf("-----------------------------------------------");
+			printf("\n");
+			printf("修改后结果为\n");
+			printf("id: %d   姓名: %s   性别: %s   专业: %s   出生日期: %s   家庭地址: %s   英语入学成绩: %f\n", item->id, item->name, item->sex, item->field, item->birthday, item->address, item->E_grade);
+			printf("\n");
+			system("pause");
+			return;
+		}
+		else
+			item = item->next;
+	}
+	printf("id不存在！返回至菜单\n");
+	system("pause");
+	return;
 }
 
 void del()//删除 
 {
-    int id;
-    struct Stu* needToDel,* last;
-    last = NULL;
-    printf("输入要删除学生的id:");
-    scanf("%d", &id);
-    needToDel = head;
-    while (needToDel != NULL)
-    {
-        if (needToDel->id == id)
-        {
-            printf("id: %d   姓名: %s   性别: %s   专业: %s   出生日期: %s   家庭地址: %s   英语入学成绩: %f\n", needToDel->id, needToDel->name, needToDel->sex, needToDel->field, needToDel->birthday, needToDel->address, needToDel->E_grade);
-            if (last == NULL) {
-                head = needToDel->next;
-                free(needToDel);
-            }
-            else {
-                last->next = needToDel->next;
-                free(needToDel);
-            }
-            printf("该学生信息已删除\n");
-            system("pause");
-            return;
-        }
-        else
-        {
-            last = needToDel;
-            needToDel = needToDel->next;
-        }
-    }
-    printf("id不存在！返回至菜单:\n");
-    system("pause");
-    return;
+	int id;
+	struct Stu* needToDel,* last;
+	last = NULL;
+	printf("输入要删除学生的id:");
+	scanf("%d", &id);
+	needToDel = head;
+	while (needToDel != NULL)
+	{
+		if (needToDel->id == id)
+		{
+			printf("id: %d   姓名: %s   性别: %s   专业: %s   出生日期: %s   家庭地址: %s   英语入学成绩: %f\n", needToDel->id, needToDel->name, needToDel->sex, needToDel->field, needToDel->birthday, needToDel->address, needToDel->E_grade);
+			if (last == NULL) {
+				head = needToDel->next;
+				free(needToDel);
+			}
+			else {
+				last->next = needToDel->next;
+				free(needToDel);
+			}
+			printf("该学生信息已删除\n");
+			system("pause");
+			return;
+		}
+		else
+		{
+			last = needToDel;
+			needToDel = needToDel->next;
+		}
+	}
+	printf("id不存在！返回至菜单:\n");
+	system("pause");
+	return;
 }
 
 void searchall()//查找全部（遍历） 
 {
-    //struct Stu* p, * q;
-    if (head == NULL)
-    {
-        return;
-    }
-    head = head->next;
-    while (head != NULL)
-    {
-        printf("id: %d   姓名: %s   性别: %s   专业: %s   出生日期: %s   家庭地址: %s   英语入学成绩: %f\n", p->id, p->name, p->sex, p->field, p->birthday, p->address, p->E_grade);
-        head = head->next;
-    }
-    printf("\n");
+	//struct Stu* p, * q;
+	if (head == NULL)
+	{
+		return;
+	}
+	head = head->next;
+	while (head != NULL)
+	{
+		printf("id: %d   姓名: %s   性别: %s   专业: %s   出生日期: %s   家庭地址: %s   英语入学成绩: %f\n", p->id, p->name, p->sex, p->field, p->birthday, p->address, p->E_grade);
+		head = head->next;
+	}
+	printf("\n");
 }
 
 void sort()
@@ -300,7 +300,7 @@ void save()
 }
 
 int main(int argc, char* argv[]) {
-    menu();
-    //system("pause");
-    return 0;
+	menu();
+	//system("pause");
+	return 0;
 }
